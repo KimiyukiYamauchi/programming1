9c7a45ef32bc654dca5ed27945cf1cc73a6e53c7
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2026-07-14 01:06:21 +0000

    動作確認

---
+++ chap03/p135.1.py
@@ -0,0 +1,7 @@
+#f = open('sample.txt', 'r')
+#data = f.readline()
+#print(data)
+#f.close()
+with open('sample.txt', 'r') as f:
+    data = f.readline()
+    print(data)
---
+++ chap03/p137.py
@@ -0,0 +1,5 @@
+with open('sample.txt', 'r') as f:
+    #lines = f.readlines()
+    lines = list(f)
+print(lines)
+
