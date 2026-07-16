26ea8c99d48922988bd38b29f1f75aae529a2df8
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2026-07-16 00:28:48 +0000

    動作確認

--- chap04/p179.3.py
+++ chap04/p179.3.py
@@ -2,5 +2,7 @@ pairs = [(2, 'down'), (1, 'up'), (4, 'charm'), (3, 'strange'), (6, 'top'), (5, '
 #print(pairs)
 #pairs.sort()
 #print(pairs)
-pairs.sort(key=lambda x: x[1])
+#pairs.sort(key=lambda x: x[1])
+#print(pairs)
+pairs.sort(key=lambda x: x[1], reverse=True)
 print(pairs)
