5bcc71e55ed0114f33387f861c693310f7471ecc
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2026-07-20 21:57:36 +0900

    動作確認

---
+++ chap05/p218.1.py
@@ -0,0 +1,4 @@
+I = 2
+J = 4
+mat = [[i * J + j + 1 for j in range(J)] for i in range(I)]
+print(mat)
---
+++ chap05/p254.1.py
@@ -0,0 +1,4 @@
+list1 = [1, 2, 3]
+list2 = ['Hokkaido', 'Aomori', 'Iwate']
+prefecture_dict = dict(zip(list1, list2))
+print(prefecture_dict)
