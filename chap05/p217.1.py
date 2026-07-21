cb005369e9fffc0df05404142a36d3abed91b054
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2026-07-21 02:51:03 +0000

    動作確認

--- chap05/p217.1.py
+++ chap05/p217.1.py
@@ -1,3 +1,5 @@
 prime = [2, 3, 5, 7, 11, 13]
-price_square = [x ** 2 for x in prime]
-print(price_square)
+prime_square = [x ** 2 for x in prime]
+print(prime_square)
+#prime_square = map(lambda x: x ** 2, prime)
+#print(list(prime_square))
--- chap05/p218.3.py
+++ chap05/p218.3.py
@@ -1,3 +1,5 @@
 name_list = [('yamada', 20), ('satou', 35), ('tanaka', 50), ('suzuki', 40)]
-indices = [i for i in range(len(name_list)) if name_list[i][1] >= 30]
-print(indices)
+#indices = [i for i in range(len(name_list)) if name_list[i][1] >= 30]
+#print(indices)
+name = [name_list[i][0] for i in range(len(name_list)) if name_list[i][1] >= 30]
+print(name)
